8970fc595826b95894ffda2a8a66fdf0f439ea04
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-08-19 18:47:21 -0300

    fix: fix more

--- simple/my-colorblocks/launch.sh
+++ simple/my-colorblocks/launch.sh
@@ -2,7 +2,7 @@
 
 # Add this script to your wm startup file.
 
-DIR="$HOME/.config/polybar/my-colorblocks"
+DIR="$HOME/.config/polybar/simple/my-colorblocks"
 
 # Terminate already running bar instances
 killall -q polybar
